a0e0336691af1653ffe846bf491267833f35ba04
kb33081024 <1318947374@qq.com>
2024-08-27 23:34:26 +0800

    update 新增user接口

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,12 +1,12 @@
 // user 数据访问层
 
 const db = require("../db")
-
+// 查询usertable
 exports.getUserInfo = (req,res)=>{
-    const {username,age,status} = req.body
-    let sql = 'select * from userTable'
+    const {username,age,role} = req.body
+    let sql = `select * from userTable where username like'%${username}%'and age like '%${age}%' and role like '%${role}%'`
+    console.dir(sql);
     db.query(sql,(err,data)=>{
-        console.log(data);
         if(err){
             return res.send('错误'+err.msg)
         }
@@ -15,5 +15,41 @@ exports.getUserInfo = (req,res)=>{
             msg:'success',
             data
         })
+    })
+}
+// 新增user数据
+exports.addUser = (req,res)=>{
+    const {username,sex,age,address,status,role,phone} = req.body
+    let querySql = `select * from userTable where username='${username}'`
+    db.query(querySql,(err,data)=>{
+        if(data.length>0){
+            res.send({
+                code:400,
+                msg:'当前用户已存在'
+            })
+        }else{
+            let sql = 'insert into userTable set ?'
+            const params = {
+                username,sex,age,address,status,role,phone
+            }
+            db.query(sql,params,(err,data)=>{
+                // console.log(data);
+                if(err){
+                    res.send({
+                        status:400,
+                        msg:err.message
+                    })
+                }else if(data.affectedRows !==1){
+                    res.send({
+                        status:400,msg:'数据写入失败'
+                    })
+                }else{
+                    res.send({
+                        status:200,
+                        msg:'新增成功'
+                    })
+                }
+            })
+        }
     })
 }
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ let express = require('express')
 let router = express.Router()
 
 const { urlencoded } = require("body-parser")
-const { getUserInfo } = require("../controllers/user.controller")
+const { getUserInfo, addUser } = require("../controllers/user.controller")
 const urlencodedParser = urlencoded({extended:false})
 
 // 获取用户信息
 
 router.post('/api/userInfo',urlencodedParser,getUserInfo)
+
+router.post('/api/addUser',urlencodedParser,addUser)
 
 module.exports = router
